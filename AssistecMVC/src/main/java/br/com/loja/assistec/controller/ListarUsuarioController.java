6f07691147b28ffc1c5f316e2046046dd14e748c
Carlos Eduardo de Souza Furtunato <Carlos.esf@aluno.ifsc.edu.br>
2024-11-13 12:04:05 -0300

    Mais coisa mudada

---
+++ AssistecMVC/src/main/java/br/com/loja/assistec/controller/ListarUsuarioController.java
@@ -0,0 +1,65 @@
+package br.com.loja.assistec.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import br.com.loja.assistec.model.Usuario;
+import br.com.loja.assistec.view.ListarUsuariosView;
+
+public class ListarUsuarioController {
+	
+	private ListarUsuariosView listarView;
+	
+	
+	public ListarUsuarioController() {
+		
+		listarView = new ListarUsuariosView();
+		listarView.setLocationRelativeTo(null);
+		listarView.setVisible(true);
+		configurarListeners();
+		carregarUsuarios();
+		
+	}
+	
+	public void configurarListeners () {
+		
+		listarView.addListarUsuariosListener(new ListarUsuariosListener());
+		listarView.addWindowListener(new JanelaAberturaListener());
+		
+		
+	}
+	
+	public void carregarUsuarios () {
+		
+		
+	}
+	private class ListarUsuariosListener implements ActionListener{
+
+	
+		public void actionPerformed(ActionEvent e) {
+			
+			String comando = e.getActionCommand();
+			switch (comando) {
+			case "BotaoCadastrarAction": 
+				
+				abrirCadastroUsuario(null);
+				break;
+				
+			case "BotaoFecharAction":
+				listarView.dispose();
+				break;
+	
+			
+			default:
+				break;
+			}
+		}
+		
+		
+	}
+	public void abrirCadastroUsuario (Usuario usuarioSelecionado) {
+		
+		//new CadastrarUsuarioController(this, usuarioSelecionado);
+		
+	}
+}
